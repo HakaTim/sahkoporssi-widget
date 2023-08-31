3d8b8aa9c2c096a1cd07751b4ceeb7ae76285d64
vili <me@vili.dev>
2023-08-31 15:41:46 +0300

    Created a installer file

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+
+# ~Vili (https://vili.dev)
+
+set -e
+
+PLASMOID_DIR=`dirname $0`/package
+plasmoidName="dev.vili.sahkoporssi"
+filename="${plasmoidName}.tar"
+cd $PLASMOID_DIR
+tar -cvf $filename *
+cd -
+mkdir -p dist
+mv $PLASMOID_DIR/$filename dist/$filename
+echo "md5: $(md5sum dist/$filename | awk '{ print $1 }')"
+echo "sha256: $(sha256sum dist/$filename | awk '{ print $1 }')"
+plasmapkg2 -i dist/$filename
+echo "Done..! Check your widget menu!"
